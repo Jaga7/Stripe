80b0aaf2389f5f1d93598e41c46d0ecfb9af018d
Andrzej J <75665204+Jaga7@users.noreply.github.com>
2022-04-20 19:14:24 +0200

    Added submenu component

--- src/Submenu.js
+++ src/Submenu.js
@@ -1,4 +1,13 @@
+import React, { useState, useRef, useEffect } from "react"
+import { useGlobalContext } from "./context"
+
 const Submenu = () => {
-  return <div>Submenu</div>
+  const { isSubmenuOpen } = useGlobalContext()
+  return (
+    <aside className={`${isSubmenuOpen ? "submenu show" : "submenu"}`}>
+      submenu
+    </aside>
+  )
 }
+
 export default Submenu
